e391c2b0d3bd5dcea69e44c636bfc0aaa41ee243
drame_c <drame_c@etna-alternance.net>
2024-09-20 15:40:21 +0200

    inci test

--- searchBar/App.js
+++ searchBar/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { SafeAreaView } from 'react-native';
 import SearchBar from './components/SearchBar';
+import InciBeauty from './components/InciBeauty';
 
 export default function App() {
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <SearchBar />
+      {/* <SearchBar /> */}
+      <InciBeauty />
     </SafeAreaView>
   );
 }
