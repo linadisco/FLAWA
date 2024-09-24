c0272fe21fe542b05ff2319d908b49bcf189bbdb
drame_c <drame_c@etna-alternance.net>
2024-09-24 16:02:29 +0200

    fix

--- searchBar/App.js
+++ searchBar/App.js
@@ -6,8 +6,8 @@ import InciBeauty from './components/InciBeauty';
 export default function App() {
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      {/* <SearchBar /> */}
-      <InciBeauty />
+      <SearchBar />
+      {/* <InciBeauty /> */}
     </SafeAreaView>
   );
 }
