38e1285dba67b2df6a817bad8e7a8e44bc950dea
drame_c <drame_c@etna-alternance.net>
2024-09-26 17:53:59 +0200

    part 1

--- api/python/fract.py
+++ api/python/fract.py
@@ -16,4 +16,10 @@ if not os.path.exists(output_dir):
     os.makedirs(output_dir)
 
 # Fractionner les données et sauvegarder dans le nouveau répertoire
-for i in range(0, len(data), lines_per_
+for i in range(0, len(data), lines_per_file):
+    split_data = data[i:i+lines_per_file]
+    output_file_path = os.path.join(output_dir, f'barcode_part_{i//lines_per_file + 11}.json')
+    with open(output_file_path, 'w') as outfile:
+        json.dump(split_data, outfile, indent=4)
+
+print("Le fichier JSON a été fractionné et enregistré dans le répertoire spécifié avec succès.")
